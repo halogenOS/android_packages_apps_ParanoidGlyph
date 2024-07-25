bac9296421c22e3262d03beb0b00777dbf65e844
N0m4n904 <noah.anleitner@halogenos.org>
2024-07-25 18:27:37 +0200

    Revert "ParanoidGlyph: Improve volume level animation"
    This reverts commit ea716304abd0a856e74368abe823ab27717310d3.

--- src/co/aospa/glyph/Manager/AnimationManager.java
+++ src/co/aospa/glyph/Manager/AnimationManager.java
@@ -198,7 +198,7 @@ public final class AnimationManager {
                         volumeArray[i] = Constants.getBrightness();
 		    }
                     updateLedFrame(volumeArray);
-                    Thread.sleep(17);
+                    Thread.sleep(16);
                 }
                 long start = System.currentTimeMillis();
                 while (System.currentTimeMillis() - start <= 1800) {
